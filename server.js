1191c363cb2161b60c65b2ba8086797059dcbb72
Jasmine <wrkerr9@gmail.com>
2022-06-14 00:05:03 -0700

    meow

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 
+const app = express();
+const PORT = 81;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})

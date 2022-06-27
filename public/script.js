45836b00e9aecff0651d00b26f819cac3bedeb1a
Jasmine <wrkerr9@gmail.com>
2022-06-26 22:06:56 -0700

    I'm back

--- public/script.js
+++ public/script.js
@@ -1,40 +1,46 @@
 const DEBUG = true;
     
-    class Card  {
-        constructor(fronttext, backtext, themecolor=null, thumbnailURL=null) {
-            this.fronttext = fronttext;
-            this.backtext = backtext;
-            this.themecolor = themecolor;
-            this.thumbnailURL = thumbnailURL;
-        }
-        render() {
-            let parentElement = document.createElement('div');
-            parentElement.classList.add('maincountainer');
-            let thecard = document.createElement('div'); // type; HTMLDivElement
-            thecard.classList.add('thecard'); 
-            let thefront = document.createElement('div');
-            thefront.textContent = this.fronttext;
-            thefront.classList.add('thefront');
-            let theback = document.createElement('div');
-            theback.textContent = this.backtext;
-            theback.classList.add('theback');
-            if(this.themecolor !== "" && this.themecolor !== null) {
-                thefront.style.backgroundColor = this.themecolor;
-            }
-            if(this.thumbnailURL !== "" && this.thumbnailURL !== null) {
-                theback.style.backgroundImage = this.thumbnailURL;
-            }
-            thecard.appendChild(thefront)
-            thecard.appendChild(theback)
-            parentElement.appendChild(thecard);
-            return parentElement;
-        }
+class Card  {
+    constructor(fronttext, backtext, themecolor=null, thumbnailURL=null) {
+        this.fronttext = fronttext;
+        this.backtext = backtext;
+        this.themecolor = themecolor;
+        this.thumbnailURL = thumbnailURL;
     }
-    window.onload = () => {
-
-        let newCard = new Card("1", "2", "pink", "");
-        let newCardDiv = newCard.render();
-        let a = Array.from(document.querySelectorAll('div')).pop();
-
-        document.querySelector('p').insertBefore(newCardDiv, undefined);
+    render() {
+        let parentElement = document.createElement('div');
+        parentElement.classList.add('maincountainer');
+        let thecard = document.createElement('div'); // type; HTMLDivElement
+        thecard.classList.add('thecard'); 
+        let thefront = document.createElement('div');
+        thefront.textContent = this.fronttext;
+        thefront.classList.add('thefront');
+        let theback = document.createElement('div');
+        theback.textContent = this.backtext;
+        theback.classList.add('theback');
+        if(this.themecolor !== "" && this.themecolor !== null) {
+            thefront.style.backgroundColor = this.themecolor;
+        }
+        if(this.thumbnailURL !== "" && this.thumbnailURL !== null) {
+            if(DEBUG) {
+                console.log(this.thumbnailURL);
+            }
+            theback.style.backgroundImage = `url('${this.thumbnailURL}'`;
+            theback.style.backgroundSize = 'contain';
+            console.log(theback);
+        }
+        thecard.appendChild(thefront)
+        thecard.appendChild(theback)
+        parentElement.appendChild(thecard);
+        return parentElement;
     }
+}
+window.onload = () => {
+
+    let newCard = new Card("Someone else's cat", "u/AC_Strand on Reddit", "pink", "./images/cat_1.jpg");
+
+    let newCardDiv = newCard.render();
+    let a = Array.from(document.querySelectorAll('div')).pop();
+
+    document.querySelector('p').insertBefore(newCardDiv, undefined);
+}
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 81;
 
-app.use(express.static('public'))
+app.use('/', express.static('public'))
+app.use('/images', express.static('images'))
 
 
 app.listen(PORT, () => {

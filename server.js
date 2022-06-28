7d1d48da04a51b81847dbdf52e71ce4bf558d73d
Jasmine <wrkerr9@gmail.com>
2022-06-27 23:34:10 -0700

    meow

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT = 81;
+const PORT = 80;
 
-app.use('/', express.static('public'))
-app.use('/images', express.static('images'))
-
+app.use('/', express.static(__dirname + '/public'))
+app.use('/images', express.static(__dirname + '/images'))
+app.use('/graphik-font-family', express.static(__dirname + '/graphik-font-family'))
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)

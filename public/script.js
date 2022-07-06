bb4206314227187c0aa212fe8e68f0c140838235
Jasmine <wrkerr9@gmail.com>
2022-07-06 12:01:01 -0700

    more flower photos

--- public/script.js
+++ public/script.js
@@ -16,7 +16,11 @@ class Card  {
         thefront.textContent = this.fronttext;
         thefront.classList.add('thefront');
         let theback = document.createElement('div');
+        if(this.backtext.includes('<a href=')) {
+            theback.innerHTML = this.backtext;
+        } else {
         theback.textContent = this.backtext;
+        }
         theback.classList.add('theback');
         if(this.themecolor !== "" && this.themecolor !== null) {
             thefront.style.backgroundColor = this.themecolor;
@@ -39,8 +43,12 @@ window.onload = () => {
 
     let newCard = new Card("Someone else's cat", "u/AC_Strand on Reddit", "pink", "./images/cat_1.jpg");
 
+    let nextCard = new Card("Photo", "Photo by <a href='https://unsplash.com/@roiboscht?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>Stephan H.</a> on <a href='https://unsplash.com/s/photos/thistle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'>Unsplash</a>",
+        "pink", "./images/marigold-unsplash.jpg");
     let newCardDiv = newCard.render();
+    let nextCardDiv = nextCard.render();
     let a = Array.from(document.querySelector('.cardgrid'));
 
     document.querySelector('.cardgrid').insertBefore(newCardDiv, undefined);
+    document.querySelector('.cardgrid').insertBefore(nextCardDiv, undefined);
 }

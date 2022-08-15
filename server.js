3d84e7c29f643c4b0e16d754ec99e117cac75d85
Jasmine <wrkerr9@gmail.com>
2022-08-15 10:45:01 -0700

    messing around

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.use('/', express.static(__dirname + '/public'))
 app.use('/images', express.static(__dirname + '/images'))
 app.use('/graphik-font-family', express.static(__dirname + '/graphik-font-family'))
 
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/public/contact.html');
+})
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
 })

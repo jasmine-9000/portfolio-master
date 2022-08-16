6d57600a58c6c0ac0d93e31fefa0fe20eca650b2
Jasmine <wrkerr9@gmail.com>
2022-08-16 13:24:12 -0700

    meow

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = 80;
+
+// express's body parser
+app.use(express.urlencoded({extended: false}))
 
 app.use('/', express.static(__dirname + '/public'))
 app.use('/images', express.static(__dirname + '/images'))
@@ -13,6 +17,34 @@ app.get('/contact', (req, res) => {
     res.render(__dirname + '/views/contact.ejs');
     //res.sendFile(__dirname + '/public/contact.html');
 })
+app.post('/contact', async (req, res) => {
+    console.log(req.body);
+    /*
+    let testAccount = await nodemailer.createTestAccount(); 
+    let transporter = nodemailer.createTransport({
+        name: "ethereal.email",
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+            user: testAccount.user,
+            pass: testAccount.pass
+        }
+    })
+    let info = await transporter.sendMail({
+        from: `wrkerr9@gmail.com`,
+        to: "wrkerr9@gmail.com",
+        subject: `Auto generated message from Localhost`,
+        text: `${req.body.first_name} ${req.body.last_name} ${req.body.message} Website: ${req.body.website ? req.body.website : ""}`,
+        html: `<p>${req.body.first_name} ${req.body.last_name} ${req.body.message} Website: ${req.body.website ? req.body.website : "n/a"}, Email: ${req.body.email ? req.body.email : "n/a"}</p>`
+    })
+    
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    */
+    res.redirect('/')
+})
+
 app.get('/', (req, res) => {
     res.render(__dirname + '/views/sales.ejs');
 })
